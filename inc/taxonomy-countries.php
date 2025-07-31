7c907c2903860e4f0f142e7273a820be5f8fc937
Artur Yeszhanov <arturyeszhanov@gmail.com>
2025-07-31 22:57:15 +0500

    Register Countries taxonomy for Cities

---
+++ inc/taxonomy-countries.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package Storefront_Child
+ * 
+ * Регистрирует пользовательскую таксономию 'Countries' для типа записи 'Cities'.
+ */
+ 
+defined( 'ABSPATH' ) || exit;
+
+add_action('init', 'storefront_child_register_taxonomy_countries');
+    
+function storefront_child_register_taxonomy_countries() {
+    $labels = [
+        'name'              => __('Countries', 'storefront-child'),
+        'singular_name'     => __('Country', 'storefront-child'),
+        'search_items'      => __('Search Countries', 'storefront-child'),
+        'all_items'         => __('All Countries', 'storefront-child'),
+        'parent_item'       => __('Parent Country', 'storefront-child'),
+        'parent_item_colon' => __('Parent Country:', 'storefront-child'),
+        'edit_item'         => __('Edit Country', 'storefront-child'),
+        'update_item'       => __('Update Country', 'storefront-child'),
+        'add_new_item'      => __('Add New Country', 'storefront-child'),
+        'new_item_name'     => __('New Country Name', 'storefront-child'),
+        'menu_name'         => __('Countries', 'storefront-child'),
+    ];    
+
+    $args = [
+        'labels'            => $labels,
+        'hierarchical'      => true,
+        'show_admin_column' => true,
+        'rewrite'           => ['slug' => 'countries'],
+        'show_in_rest'      => false, //
+    ];
+
+    register_taxonomy('countries', ['cities'], $args);
+}
+ 
